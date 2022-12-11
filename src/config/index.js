a47e0cd5f2ecc7adb9e4bdbc70e4a22341757c84
muhammadrezaa21 <mreza21031998@gmail.com>
2022-12-11 21:08:26 +0700

    ubah host url ke balancia

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,2 @@
-export const host_url = "https://fair-jade-bear-suit.cyclic.app";
-export const host_url_api = "https://fair-jade-bear-suit.cyclic.app/api/v1/";
+export const host_url = "http://balancia.co.id:4000/";
+export const host_url_api = "http://balancia.co.id:4000/api/v1/";
